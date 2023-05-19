5adecd4b0af9fa80e918ec713c8e6c5968c9cdf3
Tatiana Bryzgalova <115479875+TatianaBrt@users.noreply.github.com>
2023-05-19 19:17:51 +0300

    Frontend

---
+++ src/MyMeals.js
@@ -0,0 +1,12 @@
+import {AiFillEdit, AiFillDelete} from "react-icons/ai"
+export const MyMeals=({text,updatingInInput,deleteMeal})=>{
+    return(
+        <div>
+<p>{text}</p>
+<AiFillEdit onClick={updatingInInput}></AiFillEdit>
+<AiFillDelete onClick={deleteMeal}></AiFillDelete>
+        </div>
+    )
+}
+
+export default MyMeals;
